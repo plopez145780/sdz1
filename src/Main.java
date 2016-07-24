10ee18cf8727eb658a0a56b1b5a9ff7edbf4f351
Plopez <pierre_albert_lopez@hotmail.fr>
2016-07-24 23:40:27 +0200

    chaptitre 1ere class - partie2

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,47 @@ import java.util.Scanner;
  */
 public class Main {
 	public static void main(String[] args) {
-        Ville ville = new Ville();
+        Ville v = new Ville();
+        Ville v1 = new Ville("Marseille", 123456, "France");
+        Ville v2 = new Ville("Rio", 56789, "Brésil");
 
-        System.out.println(ville.nomVille);
-        ville.nomVille = "la tête a toto";
-        System.out.println(ville.nomVille);
+        System.out.println("\n v = "+v.getNom()+" ville de  "+v.getNombreHabitants()+ " habitants se situant en "+v.getNomPays());
+        System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
 
-        Ville ville2 = new Ville("Marseille", 123456, "France");
+        /*
+          Nous allons interchanger les Villes v1 et v2
+          tout ça par l'intermédiaire d'un autre objet Ville.
+        */
+        Ville temp = new Ville();
 
-        ville2.nomPays = "la tête a tutu";
-        System.out.println(ville2.nomPays);
+        temp = v1;
+        v1 = v2;
+        v2 = temp;
 
+        System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+
+        /*
+          Nous allons maintenant interchanger leurs noms
+          cette fois par le biais de leurs mutateurs.
+        */
+
+        v1.setNom("Hong Kong");
+        v2.setNom("Djibouti");
+
+        System.out.println(" v1 = " + v1.getNom() + " ville de  " + v1.getNombreHabitants() + " habituant se situant en " + v1.getNomPays());
+        System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+
+        //-----------------------------
+
+        Ville v00 = new Ville();
+        Ville v01 = new Ville("Marseille", 1236, "France");
+        Ville v02 = new Ville("Rio", 321654, "Brésil");
+
+        System.out.println("\n\n"+v01.decrisToi());
+        System.out.println(v00.decrisToi());
+        System.out.println(v02.decrisToi()+"\n\n");
+        System.out.println(v01.comparer(v02));
     }
 }
--- src/Ville.java
+++ src/Ville.java
@@ -6,6 +6,7 @@ public class Ville {
     private String nomVille;
     private int nbreHabitants;
     private String nomPays;
+    private char categorie;
 
     //controlleurs
     public Ville(){
@@ -13,6 +14,7 @@ public class Ville {
         nomVille = "Inconnu";
         nomPays = "Inconnue";
         nbreHabitants = 0;
+        this.setCategorie();
     }
 
     public Ville(String pNom, int pNbre, String pPays){
@@ -20,31 +22,60 @@ public class Ville {
         nomVille = pNom;
         nomPays = pPays;
         nbreHabitants = pNbre;
+        this.setCategorie();
     }
 
     //getter(asseceurs) / setter(mutateurs)
 
-    public String getNomVille() {
+    public String getNom() {
         return nomVille;
     }
 
-    public int getNbreHabitants() {
+    public int getNombreHabitants() {
         return nbreHabitants;
     }
 
     public String getNomPays() {
         return nomPays;
     }
-
-    public void setNomVille(String nomVille) {
-        this.nomVille = nomVille;
+    public char getCategorie(){
+        return categorie;
     }
 
-    public void setNbreHabitants(int nbreHabitants) {
-        this.nbreHabitants = nbreHabitants;
+    public void setNom(String pNom) {
+        this.nomVille = pNom;
     }
 
-    public void setNomPays(String nomPays) {
-        this.nomPays = nomPays;
+    public void setNombreHabitants(int nbre) {
+        this.nbreHabitants = nbre;
+        this.setCategorie();
+    }
+
+    public void setNomPays(String pPays) {
+        this.nomPays = pPays;
+    }
+
+    //Méthodes d'instance
+    private void setCategorie(){
+        int bornesSuperieures[] = {0, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000};
+        char categories[] = {'?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+        int i = 0;
+        while (i < bornesSuperieures.length && this.nbreHabitants > bornesSuperieures[i])
+            i++;
+        this.categorie = categories[i];
+    }
+
+    public String decrisToi(){
+        return "\t"+this.nomVille+" est une ville de "+this.nomPays+ ", elle comporte : "+this.nbreHabitants+" habitant(s) => elle est donc de catégorie : "+this.categorie;
+    }
+
+    public String comparer(Ville v1){
+        String str = new String();
+
+        if (v1.getNombreHabitants() > this.nbreHabitants)
+            str = v1.getNom()+" est une ville plus peuplée que "+this.nomVille;
+        else
+            str = this.nomVille+" est une ville plus peuplée que "+v1.getNom();
+        return str;
     }
 }
