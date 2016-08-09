4ddd7115c5c28d2f5a26e6eb6dd57d63527398b0
plopez <pierre_albert_lopez@hotmail.fr>
2016-08-09 15:52:09 +0200

    Nettoyage projet

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
-import com.sdz.comportement.*;
+
 /**
  * Created by pierr on 05/07/2016.
  */
 public class Main {
     public static void main(String[] args){
-        Personnage[] tPers = {new Guerrier(), new Civil(), new Medecin()};
 
-        for(int i = 0; i < tPers.length; i++){
-            System.out.println("\nInstance de " + tPers[i].getClass().getName());
-            System.out.println("*****************************************");
-            tPers[i].combattre();
-            tPers[i].seDeplacer();
-            tPers[i].soigner();
-        }
-
-        System.out.println("*****************************************");
-        System.out.println("*****************************************");
-        Personnage pers = new Guerrier();
-        pers.soigner();
-        pers.setSoin(new Operation());
-        pers.soigner();
     }
 }
--- src/com/sdz/comportement/EspritCombatif.java
+++
@@ -1,8 +0,0 @@
-package com.sdz.comportement;
-
-/**
- * Created by pierr on 02/08/2016.
- */
-public interface EspritCombatif {
-    void combat();
-}
