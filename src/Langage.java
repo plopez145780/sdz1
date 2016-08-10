f0f6474e4b71e4518d9d448b69a8cb02cce85e94
plopez <pierre_albert_lopez@hotmail.fr>
2016-08-10 14:32:32 +0200

    Chapitre enumération

---
+++ src/Langage.java
@@ -0,0 +1,13 @@
+/**
+ * Description :
+ * Date : 09/08/2016
+ *
+ * @author pierr
+ * @version 1.0
+ */
+public enum Langage {
+    JAVA,
+    C,
+    CplusPlus,
+    PHP;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 /**
- * Created by pierr on 05/07/2016.
+ * Description :
+ * Date : 09/08/2016
+ *
+ * @author pierr
+ * @version 1.0
  */
 public class Main {
-	public static void main(String[] args) {
-            Ville v = null;
-            try {
-                    v = new Ville("Re", 12000, "France");
-            }
-            //Gestion de plusieurs exceptions différentes
-            catch (NombreHabitantException | NomVilleException e2){
-                System.out.println(e2.getMessage());
-            }
-            finally{
-                if(v == null)
-                    v = new Ville();
-            }
-            System.out.println(v.decrisToi());
+    public static void main(String args[]){
+        for(Langage lang : Langage.values()){
+            if(Langage.JAVA.equals(lang))
+                System.out.println("J'aime le : " + lang);
+            else
+                System.out.println(lang);
+        }
     }
 }
