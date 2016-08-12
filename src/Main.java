d6fe323e600a63c6328fc05d4e900f8a6d438546
Plopez <pierre_albert_lopez@hotmail.fr>
2016-08-12 10:50:44 +0200

    Chapitre IO - lecture/ecriture BufferedInputStream/BufferedOutputStream

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 
 public class Main {
     public static void main(String[] args){
         FileInputStream fis = null;
+        FileOutputStream fos = null;
         BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
 
         try{
             fis = new FileInputStream(new File("test.txt"));
@@ -20,14 +21,25 @@ public class Main {
                     )
             );
 
+            fos = new FileOutputStream(new File("test2.txt"));
+            bos = new BufferedOutputStream(
+                    new FileOutputStream(
+                            new File("test3.txt")
+                    )
+            );
+
             byte[] buf = new byte[8];
             long startTime = System.currentTimeMillis();
-            while(fis.read(buf) != -1);
-            System.out.println("Temps de lecture avec FileInputStream : " + (System.currentTimeMillis() - startTime));
+            while(fis.read(buf) != -1){
+                fos.write(buf);
+            }
+            System.out.println("Temps de lecture/ecriture avec FileInputStream/FileOutputStream : " + (System.currentTimeMillis() - startTime));
 
             startTime = System.currentTimeMillis();
-            while(bis.read(buf) != -1);
-            System.out.println("Temps de lecture avec BufferedInputStream : " + (System.currentTimeMillis() - startTime));
+            while(bis.read(buf) != -1){
+                bos.write(buf);
+            }
+            System.out.println("Temps de lecture/ecriture avec BufferedInputStream/BufferedOutputStream : " + (System.currentTimeMillis() - startTime));
         }
         catch(FileNotFoundException e){
             e.printStackTrace();
@@ -48,6 +60,24 @@ public class Main {
             try{
                 if(bis != null){
                     bis.close();
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+
+            try{
+                if(bos != null){
+                    bos.close();
                 }
             }
             catch (IOException e){
