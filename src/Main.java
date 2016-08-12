3d7b4f6bc97fd339518301de7f608cb36a572f83
Plopez <pierre_albert_lopez@hotmail.fr>
2016-08-12 09:43:57 +0200

    Chapitre IO - file

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+
 /**
  * Created by pierr on 11/08/2016.
  */
 public class Main {
     public static void main(String[] args){
-        Solo<Integer> val = new Solo<Integer>();
-        Solo<String> valS = new Solo<String>("TOTOTOTO");
-        Solo<Float> valF = new Solo<Float>(12.2f);
-        Solo<Double> valD = new Solo<Double>(12.202568);
 
-        Duo<String, Boolean> dual = new Duo<>("toto", true);
-        System.out.println("valeur de l'objet dual : val1 = " + dual.getValeur1() + ", val2 = " + dual.getValeur2());
+        File f = new File("IO/test.txt");
 
-        Duo<Double, Character>dual2 = new Duo<>(12.2585, 'C');
-        System.out.println("Valeur de l'objet dual : val1 = " + dual2.getValeur1() + ", val2 = " + dual2.getValeur2());
+        System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
+        System.out.println("Nom du fichier : " + f.getName());
+        System.out.println("Est-ce qu'il existe ? " + f.exists());
+        System.out.println("Est-ce un répertoire ? " + f.isDirectory());
+        System.out.println("Est-ce un fichier ? " + f.isFile());
 
+        System.out.println("Affichage des lecteurs a la racine du PC : ");
+        for(File file : f.listRoots()){
+            System.out.println(file.getAbsolutePath());
+            try{
+                int i = 1;
+                for (File nom : file.listFiles()){
+                    System.out.println("\t\t" + ((nom.isDirectory()) ? nom.getName() + "/" : nom.getName()));
 
-        System.out.println("Liste de String");
-        System.out.println("------------------------------");
-        List<String> listeString= new ArrayList<String>();
-        listeString.add("Une chaîne");
-        listeString.add("Une autre");
-        listeString.add("Encore une autre");
-        listeString.add("Allez, une dernière");
+                    if((i%4) == 0){
+                        System.out.print("\n");
+                    }
+                    i++;
+                }
+                System.out.println("\n");
+            }
+            catch(NullPointerException e){
 
-        for(String str : listeString)
-            System.out.println(str);
-
-        System.out.println("\nListe de float");
-        System.out.println("------------------------------");
-
-        List<Float> listeFloat = new ArrayList<Float>();
-        listeFloat.add(12.25f);
-        listeFloat.add(15.25f);
-        listeFloat.add(2.25f);
-        listeFloat.add(128764.25f);
-
-        for(float f : listeFloat)
-            System.out.println(f);
-
-
-
+            }
+        }
     }
 }
