16f912d7b4c55fb99d67e478e89e7bbdec687d96
Plopez <pierre_albert_lopez@hotmail.fr>
2016-08-12 17:06:49 +0200

    Chapitre IO - File(Writer/Reader) et Print(Writer/Reader)

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,32 @@
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
-
-        CharArrayWriter caw = new CharArrayWriter();
-        CharArrayReader car = null;
+        File file = new File("testFileWriter.txt");
+        FileWriter fw;
+        FileReader fr;
 
         try{
-            caw.write("Coucou les zéros");
-            System.out.println(caw);
-            caw.close();
+            fw = new FileWriter(file);
+            String str = "Bonjour à tous, amis Zéros !\n";
+            str += "\tComment allez-vous ? \n";
 
-            car = new CharArrayReader(caw.toCharArray());
-            int i;
-            String str = "";
+            fw.write(str);
+            fw.close();
 
-            while((i = car.read()) != -1){
+            fr = new FileReader(file);
+            str = "";
+            int i = 0;
+            while((i = fr.read()) != -1)
                 str += (char)i;
-            }
             System.out.println(str);
-        }
-        catch(IOException e){
+
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-
-
-
-
-        StringWriter sw = new StringWriter();
-        StringReader sr;
-
-        try {
-            sw.write("Coucou les Zéros");
-            //Appel à la méthode toString de notre objet de manière tacite
-            System.out.println(sw);
-
-            //caw.close() n'a aucun effet sur le flux
-            //Seul caw.reset() peut tout effacer
-            sw.close();
-
-            //On passe un tableau de caractères à l'objet qui va lire le tampon
-            sr = new StringReader(sw.toString());
-            int i ;
-            //On remet tous les caractères lus dans un String
-            String str = "";
-            while(( i = sr.read()) != -1)
-                str += (char) i;
-
-            System.out.println(str);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
