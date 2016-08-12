ca96ce0fe43328e6defe99bf7ec82d74a2955244
Plopez <pierre_albert_lopez@hotmail.fr>
2016-08-12 17:28:15 +0200

    Chapitre IO - nio

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,48 @@
+//Packages à importer afin d'utiliser l'objet File
+import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
 
 public class Main {
-    public static void main(String[] args){
-        File file = new File("testFileWriter.txt");
-        FileWriter fw;
-        FileReader fr;
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("test.txt");
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             FileChannel fc = fis.getChannel()){
 
-        try{
-            fw = new FileWriter(file);
-            String str = "Bonjour à tous, amis Zéros !\n";
-            str += "\tComment allez-vous ? \n";
+            //Démarrage du chrono
+            long time = System.currentTimeMillis();
+            //Lecture
+            while(bis.read() != -1);
+            //Temps d'exécution
+            System.out.println("Temps d'exécution avec un buffer conventionnel : " + (System.currentTimeMillis() - time));
 
-            fw.write(str);
-            fw.close();
+            //Création d'un nouveau flux de fichier
+            //fis = new FileInputStream(new File("test.txt"));
+            //On récupère le canal
+            //fc = fis.getChannel();
+            //On en déduit la taille
+            int size = (int)fc.size();
+            //On crée un buffer correspondant à la taille du fichier
+            ByteBuffer bBuff = ByteBuffer.allocate(size);
 
-            fr = new FileReader(file);
-            str = "";
-            int i = 0;
-            while((i = fr.read()) != -1)
-                str += (char)i;
-            System.out.println(str);
+            //Démarrage du chrono
+            time = System.currentTimeMillis();
+            //Démarrage de la lecture
+            fc.read(bBuff);
+            //On prépare à la lecture avec l'appel à flip
+            bBuff.flip();
+            //Affichage du temps d'exécution
+            System.out.println("Temps d'exécution avec un nouveau buffer : " + (System.currentTimeMillis() - time));
+
+            //Puisque nous avons utilisé un buffer de byte afin de récupérer les données
+            //Nous pouvons utiliser un tableau de byte
+            //La méthode array retourne un tableau de byte
+            byte[] tabByte = bBuff.array();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
