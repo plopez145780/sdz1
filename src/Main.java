32724db93072f060f3408321f0842611749672e8
Plopez <pierre_albert_lopez@hotmail.fr>
2016-08-13 17:07:42 +0200

    Chapitre réflexivité - instanciation dynamique

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,40 @@
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
-        String str = new String("Bonjour");
+        String nom = Paire.class.getName();
+        try{
+            Class cl = Class.forName(nom);
+            Object o = cl.newInstance();
 
-        Class c = String.class;
-        Class c2 = new String().getClass();
+            Class[] types = new Class[]{String.class, String.class};
+            Constructor ct = cl.getConstructor(types);
 
-        System.out.println("La superclasse de la classe " + String.class.getName() + " est : " + String.class.getSuperclass());
-        System.out.println("La superclasse de la classe " + Object.class.getName() + " est : " + String.class.getSuperclass());
+            Object o2 = ct.newInstance(new String[]{"valeur 1", "valeur 2"});
 
-        String str1 = new String();
-        Class c3 = str1.getClass();
-        //Class c3 = String.class; est équivalent
+            Method m = cl.getMethod("toString", null);
 
-        Class[] faces = c3.getInterfaces();
+            System.out.println("----------------------------------------");
+            System.out.println("Méthode " + m.getName() + " sur o2: " +m.invoke(o2, null));
+            System.out.println("Méthode " + m.getName() + " sur o: " +m.invoke(o, null));
 
-        System.out.println("Il y a " + faces.length + " interfaces implémentées");
-        for (Class face : faces) {
-            System.out.println(face);
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
-
-
-        Class c4 = String.class;
-        Method[] m = c4.getMethods();
-        System.out.println("Il y a " + m.length + " méthodes dans cette classe");
-        //On parcourt le tableau de méthodes
-        for(int i = 0; i < m.length; i++)
-            System.out.println(m[i]);
-
-
-
-
-        Class c5 = new String().getClass();
-        Method[] m2 = c.getMethods();
-
-        System.out.println("Il y a " + m2.length + " méthodes dans cette classe");
-        //On parcourt le tableau de méthodes
-        for(int i = 0; i < m2.length; i++)
-        {
-            System.out.println(m2[i]);
-
-            Class[] p = m2[i].getParameterTypes();
-            for(int j = 0; j < p.length; j++)
-                System.out.println(p[j].getName());
-
-            System.out.println("----------------------------------\n");
-        }
-
-
-
-
-        Class c6 = String.class;
-        Field[] f = c6.getDeclaredFields();
-        System.out.println("Il y a " + f.length + " champs dans cette classe");
-        for(int i = 0; i < f.length; i++)
-            System.out.println(f[i].getName());
-
-
-
-        Class c7 = new String().getClass();
-        Constructor[] construc = c7.getConstructors();
-        System.out.println("Il y a " + construc.length + " constructeurs dans cette classe");
-        //On parcourt le tableau des constructeurs
-        for(int i = 0; i < construc.length; i++){
-            System.out.println(construc[i].getName());
-
-            Class[] param = construc[i].getParameterTypes();
-            for(int j = 0; j < param.length; j++)
-                System.out.println(param[j]);
-
-            System.out.println("-----------------------------\n");
-        }
-
-
-
     }
 }
---
+++ src/Paire.java
@@ -0,0 +1,35 @@
+/**
+ * Created by pierr on 13/08/2016.
+ */
+public class Paire {
+    private String valeur1;
+    private String valeur2;
+
+    public Paire(){
+        this.valeur1 = null;
+        this.valeur2 = null;
+        System.out.println("Instanciation !");
+    }
+    public Paire(String val1, String val2){
+        this.valeur1 = val1;
+        this.valeur2 = val2;
+        System.out.println("Instanciation avec des paramètres !");
+    }
+
+    public String getValeur1() {
+        return valeur1;
+    }
+    public void setValeur1(String valeur1) {
+        this.valeur1 = valeur1;
+    }
+    public String getValeur2() {
+        return valeur2;
+    }
+    public void setValeur2(String valeur2) {
+        this.valeur2 = valeur2;
+    }
+
+    public String toString() {
+        return  "Je suis un objet qui a pour valeur : " + this.valeur1 +  " - " + this.valeur2;
+    }
+}
